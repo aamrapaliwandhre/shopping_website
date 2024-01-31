1cb6edf892eeb3d7d04bb0ab335aa3fa23ba84c7
Aamrapali  Wandhre <rnwandhre12@gmail.com>
2024-01-31 13:55:19 -0800

    Navbar Added
    navbar items and css for navbar

--- my-app/src/Componenets/Assests/all_product.js
+++ my-app/src/Componenets/Assests/all_product.js
@@ -35,6 +35,9 @@ import p34_img from "./product_34.png";
 import p35_img from "./product_35.png";
 import p36_img from "./product_36.png";
 
+
+//products are in json format
+
 let all_product = [
   {
     id: 1,
