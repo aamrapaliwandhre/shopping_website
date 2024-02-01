4d25c517bf3325e71a1f3f92e48962cde67a5c6c
Aamrapali  Wandhre <rnwandhre12@gmail.com>
2024-02-01 10:50:24 -0800

    Added:Login Component

--- my-app/src/Pages/LoginSingup.jsx
+++ my-app/src/Pages/LoginSingup.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
+import './CSS/loginsignup.css'
 
 const LoginSingup = () => {
   return (
-    <div>LoginSingup</div>
+    <div className='loginsignup'>
+      <div className="loginsignup-container">
+        <h1>Sign Up</h1>
+        <div className="loginsignup-fields">
+          <input type="text" placeholder='your name' name="" id="" />
+          <input type="email" placeholder='Email Address' name="" id="" />
+          <input type="password" placeholder='password' name="" id="" />
+        </div>
+        <button>Continue</button>
+        <p className='loginsignup-login'>Already have an account? <span>Login here</span></p>
+        <div className="loginsignup-agree">
+          <input type="checkbox" name="" id="" />
+          <p>By continuing,  I agree to terms & privacy policy.</p>
+        </div>
+      </div>
+    </div>
   )
 }
 
