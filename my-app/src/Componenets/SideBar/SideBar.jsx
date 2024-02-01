76255c62c8b37a12bd0124becfca4153bbd6d3bf
Aamrapali  Wandhre <rnwandhre12@gmail.com>
2024-02-01 10:49:36 -0800

    Added : SideBar for Filtering

---
+++ my-app/src/Componenets/SideBar/SideBar.jsx
@@ -0,0 +1,20 @@
+import Category from "./Category/Category";
+import Price from "./Price/Price";
+// import Colors from "./Colors/Colors";
+import "./SideBar.css";
+
+const Sidebar = ({ handleChange }) => {
+  return (
+    <>
+      <section className="sidebar">
+        <div className="logo-container">
+        </div>
+        <Category handleChange={handleChange} />
+        <Price handleChange={handleChange} />
+        {/* <Colors handleChange={handleChange} /> */}
+      </section>
+    </>
+  );
+};
+
+export default Sidebar;
